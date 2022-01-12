6d758139d76a7d3217036c78546da37ae49e01b4
Anand <anand@Anands-MacBook-Air.local>
2022-01-13 00:41:21 +0530

    Coffee Init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <h2 className='my-8 text-4xl font-bold'>Under Development!</h2>
+      <div className='links'>
+        <a href='https://anandjeechoubey.netlify.app/'>Home</a>
+        <a href='https://github.com/Anandjeechoubey/'>Github</a>
+        <a href='https://www.linkedin.com/in/anand-jee-choubey/'>LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
